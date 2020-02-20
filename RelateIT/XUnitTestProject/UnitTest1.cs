2ba2edac30f568a398bc2284a74c7dfed9127e8f
Kasper <kasp724s@edu.ucl.dk>
2020-02-20 09:36:47 +0100

    Setup library test project

---
+++ RelateIT/XUnitTestProject/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using RelateIT.Droid;
+using RelateIT.iOS;
+using RelateIT.UWP;
+
+namespace XUnitTestProject
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ XUnitTestingLibrary/RelateITTest.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace XUnitTestingLibrary
+{
+    public class RelateITTest
+    {
+    }
+}
