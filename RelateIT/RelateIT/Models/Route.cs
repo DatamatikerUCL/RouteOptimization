86e6e2842e300904c57b29bbef73799d3e1fefc0
Kasper <kasp724s@edu.ucl.dk>
2020-02-27 09:01:26 +0100

    create route model

---
+++ RelateIT/RelateIT/Models/Route.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace RelateIT.Models
+{
+    public class Route
+    {
+        public Point StartLocation { get; set; }
+        public Point EndLocation { get; set; }
+        public List<Point> Points { get; set; }
+        public string EstimatedTime { get; set; }
+
+
+    }
+}
