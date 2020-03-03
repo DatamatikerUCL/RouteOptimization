3a16a0058cf746d603151bbb4808a8fbb35b669d
Kaare Veggerby Sandbøl <kaar1498@edu.eal.dk>
2020-03-03 11:15:06 +0100

    Cleaned Up Folder

--- RoutePlanner/Interfaces/IDistanceCalculator.cs
+++
@@ -1,9 +0,0 @@
-using RouteOptimization.RoutePlanner.Datastructures;
-
-namespace RouteOptimization.RoutePlanner.Interfaces
-{
-    public interface IDistanceCalculator
-    {
-        double CalculateDistanceBetweenLocations(ILocateable firstLocation, ILocateable secondLocation);
-    }
-}
