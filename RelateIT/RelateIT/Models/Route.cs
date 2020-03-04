9ac73b0747a6dcb501e7f38a7c6f92ce8e443b22
Kasper <kasp724s@edu.ucl.dk>
2020-03-04 10:42:07 +0100

    Route implements IPlannable

--- RelateIT/RelateIT/Models/Route.cs
+++ RelateIT/RelateIT/Models/Route.cs
@@ -1,19 +1,28 @@
-﻿using System;
+﻿using RouteOptimization.RoutePlanner.Datastructures;
+using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Text;
 using Xamarin.Forms;
 
 namespace RelateIT.Models
 {
-    public class Route
+    public class Route : IPlannable
     {
         public string Name { get; set; }
-        public Point StartLocation { get; set; }
-        public Point EndLocation { get; set; }
-        public List<Point> Points { get; set; }
-        public int EstimatedTime { get; set; }
+
+        public ImmutableList<ILocateable> Locations { get; }
+
+        public int LocationCount { get => Locations.Count;}
+
+        public ILocateable StartLocation {get => Locations[0]; }
 
 
+        public Route(string _name, ImmutableList<ILocateable> _locations, int _locationCount, ILocateable _startLocation)
+        {
+            Name = _name;
+            Locations = _locations;
 
+        }
     }
 }
