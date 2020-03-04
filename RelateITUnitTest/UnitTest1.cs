e0c374367f4f58fe1835f66561fd45ed02b93d03
LAPTOP-4LGA03E1\kaspe <kasp724s@edu.ucl.dk>
2020-03-04 09:13:59 +0100

    Begin making tests for app

--- RelateITUnitTest/UnitTest1.cs
+++ RelateITUnitTest/UnitTest1.cs
@@ -1,11 +1,13 @@
 using System;
 using Xunit;
 using RelateIT.Droid;
+using RelateIT.Models;
 
 namespace RelateITUnitTest
 {
     public class UnitTest1
     {
+        Route route = new Route();
         [Fact]
         public void Test1()
         {
