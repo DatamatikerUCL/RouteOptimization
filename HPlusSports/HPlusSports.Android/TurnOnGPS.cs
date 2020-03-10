5d838d29bc91ec8ab11f49af1e35124b13994da0
ande714b <43536499+ande714b@users.noreply.github.com>
2020-03-10 11:16:56 +0100

    asd
    sfd

--- HPlusSports/HPlusSports.Android/TurnOnGPS.cs
+++ HPlusSports/HPlusSports.Android/TurnOnGPS.cs
@@ -5,10 +5,13 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Gms.Common.Apis;
+using Android.Gms.Location;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Xamarin.Forms;
 
 [assembly: Xamarin.Forms.Dependency(typeof(HPlusSports.Droid.TurnOnGPS))]
 
@@ -16,9 +19,36 @@ namespace HPlusSports.Droid
 {
     public class TurnOnGPS : ITurnOnGPS
     {
-        public void turnOnGPS()
+        public async void turnOnGPS()
         {
-            
+            try
+            {
+                MainActivity activity = Forms.Context as MainActivity;
+
+                GoogleApiClient googleApiClient = new GoogleApiClient.Builder(activity)
+                    .AddApi(LocationServices.API).Build();
+                googleApiClient.Connect();
+                LocationRequest locationRequest = LocationRequest.Create();
+                locationRequest.SetPriority(LocationRequest.PriorityHighAccuracy);
+                locationRequest.SetInterval(10000);
+                locationRequest.SetFastestInterval(10000 / 2);
+
+                LocationSettingsRequest.Builder
+                        locationSettingsRequestBuilder = new LocationSettingsRequest.Builder()
+                        .AddLocationRequest(locationRequest);
+                locationSettingsRequestBuilder.SetAlwaysShow(false);
+                LocationSettingsResult locationSettingsResult = await LocationServices.SettingsApi.CheckLocationSettingsAsync(
+                    googleApiClient, locationSettingsRequestBuilder.Build());
+
+                if (locationSettingsResult.Status.StatusCode == LocationSettingsStatusCodes.ResolutionRequired)
+                {
+                    locationSettingsResult.Status.StartResolutionForResult(activity, 0);
+                }
+            }
+            catch (Exception ex)
+            {
+               // GlobalVariables.SendExceptionReport(ex);
+            }
         }
     }
 }
