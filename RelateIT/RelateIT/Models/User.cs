1d68c27b62ce698873bf1635c26d6d2ad0bc87d7
metodian <anders.f.rasmussen@hotmail.com>
2020-03-27 14:06:23 +0100

    shell stuff and a login page

---
+++ RelateIT/RelateIT/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RelateIT.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public bool CheckInformation()
+        {
+            if ( !string.IsNullOrEmpty(Username) && !string.IsNullOrEmpty(Password))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ RelateIT/RelateIT/Views/LoginPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using RelateIT.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RelateIT.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Btn_Signin_Clicked(object sender, EventArgs e)
+        {
+            User user = new User(Entry_Username.Text, Entry_Password.Text);
+            if (user.CheckInformation())
+            {
+                await DisplayAlert("Login", "Login Success", "OK");
+                Application.Current.MainPage = new AppShell();
+            }
+            else
+            {
+               await DisplayAlert("Login", "Login not correct, empty username or password", "OK");
+            }
+        }
+    }
+}
