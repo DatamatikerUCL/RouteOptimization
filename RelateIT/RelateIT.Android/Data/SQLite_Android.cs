0173d0bc8dddbddfcc7a5d40ce5e6a7bdaf65d9f
metodian <anders.f.rasmussen@hotmail.com>
2020-03-31 10:41:59 +0200

    asd

--- RelateIT/RelateIT.Android/Data/SQLite_Android.cs
+++ RelateIT/RelateIT.Android/Data/SQLite_Android.cs
@@ -9,11 +9,22 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using RelateIT.Data;
+using SQLite;
 
 namespace RelateIT.Droid.Data
 {
     public class SQLite_Android : ISQLite
     {
+        public SQLite_Android()
+        {
+
+        }
+        public SQLiteConnection GetConnection()
+        {
+            var sqliteFireName = "TestDB.db3";
+            string documentPrath = System.Environment.GetFolderPath
+        }
         
     }
 }
