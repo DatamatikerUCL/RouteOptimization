4845ef166a82a8800fa4598d778f055ef40b47c4
Johannes Thomsen <43598221+joha321j@users.noreply.github.com>
2020-04-15 08:22:27 +0200

    Add SimpleChristofides class

---
+++ RoutePlanning/RoutePlanningAlgorithms/BruteForce/BruteForce.cs
@@ -0,0 +1,23 @@
+﻿using RouteOptimization.RoutePlanning.Datastructures;
+using RouteOptimization.RoutePlanning.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoutePlanning.RoutePlanningAlgorithms.BruteForce
+{
+    public class BruteForce : IRoutePlanner
+    {
+        private readonly IDistanceCalculator _distanceCalculator;
+
+        public BruteForce(IDistanceCalculator distanceCalculator)
+        {
+            _distanceCalculator = distanceCalculator;
+        }
+
+        public IPlannable PlanIPlannable(IPlannable plannable, IPlannableFactory factory)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RoutePlanning/RoutePlanningAlgorithms/SimpleChristofidesAlgorithm/SimpleChristofides.cs
@@ -0,0 +1,30 @@
+﻿using RouteOptimization.RoutePlanning.Datastructures;
+using RouteOptimization.RoutePlanning.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RoutePlanning.RoutePlanningAlgorithms.SimpleChristofidesAlgorithm
+{
+    public class SimpleChristofides : IRoutePlanner
+    {
+        public IPlannable PlanIPlannable(IPlannable plannable, IPlannableFactory factory)
+        {
+            // Select Root vertex
+            ILocateable root = GetRoot(plannable);
+            
+            // Computer minnimum spanning tree T for G with root r using Prim's.
+            
+
+            // Let H be a list of vertices ordered according to when they are first visited in a preorder tree walk of T.
+
+            // Return H
+            throw new NotImplementedException();
+        }
+
+        private ILocateable GetRoot(IPlannable plannable)
+        {
+            return plannable.StartLocation;
+        }
+    }
+}
